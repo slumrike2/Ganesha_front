d1773f620f5b098a899c4c42167b2e14c38a29f6
Silva Bouchard, Jesus Alejandro <jasilva.22@est.ucab.edu.ve>
2025-01-03 19:41:38 -0400

    Inicializacion del proyecto

---
+++ ganesha_frontend/README.md
@@ -0,0 +1,3 @@
+# ganesha_frontend
+
+A new Flutter project.
---
+++ ganesha_frontend/android/app/src/main/kotlin/com/example/ganesha_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ganesha_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
