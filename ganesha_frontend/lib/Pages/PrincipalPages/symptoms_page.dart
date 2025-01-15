6566733505a0965cc521bf211c76d7aa390db728
Silva Bouchard, Jesus Alejandro <jasilva.22@est.ucab.edu.ve>
2025-01-15 16:37:23 -0400

    a

--- ganesha_frontend/lib/Pages/PrincipalPages/symptoms_page.dart
+++ ganesha_frontend/lib/Pages/PrincipalPages/symptoms_page.dart
@@ -3,7 +3,7 @@ import 'package:ganesha_frontend/Components/symptom_test.dart';
 import 'package:ganesha_frontend/dartTypes.dart';
 
 class SymptomsPage extends StatefulWidget {
-  const SymptomsPage({super.key});
+  SymptomsPage({super.key});
 
   @override
   State<SymptomsPage> createState() => _SymptomsPageState();
@@ -15,61 +15,27 @@ class _SymptomsPageState extends State<SymptomsPage> {
     return Container(
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Spacer(),
-              Expanded(
-                flex: 2,
-                child: Text(
-                  'Sintomas',
-                  style: TextStyle(fontSize: 32, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.list, color: Colors.white),
-                        onPressed: () {
-                          // Handle list view action
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.grid_view, color: Colors.white),
-                        onPressed: () {
-                          // Handle grid view action
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          Text(
+            'Sintomas',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+            textAlign: TextAlign.center,
           ),
           Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                clipBehavior: Clip.antiAlias,
-                itemCount: 15,
-                itemBuilder: (context, index) {
-                  return SymptomTest(
-                    extended: false,
-                    data: Sintoma(
-                      nombre: 'Sintoma $index',
-                      descripcion: 'Descripcion del sintoma $index',
-                      idSintoma: index,
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
+              child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: ListView.builder(
+                    clipBehavior: Clip.antiAlias,
+                    itemCount: 15,
+                    itemBuilder: (context, index) {
+                      return SymptomTest(
+                        data: Sintoma(
+                          nombre: 'Sintoma $index',
+                          descripcion: 'Descripcion del sintoma $index',
+                          idSintoma: index,
+                        ),
+                      );
+                    },
+                  ))),
           ElevatedButton(onPressed: () {}, child: Text('Submit'))
         ],
       ),
