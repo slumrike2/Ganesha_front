2e43f2384f2843f70c8d97f18d00d6294c97eb28
aepatino21 <aepatino.21@est.ucab.edu.ve>
2025-01-23 01:25:02 -0400

    Fix: size & uppercase add to the user_avatar inside confirmation page

--- ganesha_frontend/lib/Pages/PrincipalPages/gift_confirmation_page.dart
+++ ganesha_frontend/lib/Pages/PrincipalPages/gift_confirmation_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ganesha_frontend/Components/user_avatar.dart';
 
 class GiftConfirmationPage extends StatelessWidget {
   final String friendName;
@@ -21,16 +20,22 @@ class GiftConfirmationPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
+            SizedBox(
               width: 100.0, // Set the width for the larger avatar
               height: 100.0, // Set the height for the larger avatar
-              child: UserAvatar(name: friendName),
+              child: CircleAvatar(
+                radius: 50.0,
+                child: Text(
+                  friendName[0].toUpperCase(), // Display the first letter of the name in uppercase
+                  style: TextStyle(fontSize: 60.0), // Larger font size for the character
+                ),
+              ),
             ),
             SizedBox(height: 16),
             Text(
               '¿Quieres regalar "$songTitle" a $friendName?',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
+              style: TextStyle(fontSize: 14), // Smaller font size for the text
             ),
             SizedBox(height: 32),
             Row(
