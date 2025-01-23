54702e48d5a2d5761321bcb5b653aee49ee70e06
wincancode <erodriguez.win@gmail.com>
2025-01-23 03:08:40 -0400

    added function to search and add friends

--- ganesha_frontend/lib/Pages/PrincipalPages/friends_page.dart
+++ ganesha_frontend/lib/Pages/PrincipalPages/friends_page.dart
@@ -7,12 +7,14 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Friend {
   final String name;
+  final String userid;
   final int score;
 
-  Friend({required this.name, required this.score});
+  Friend({required this.name, required this.score, required this.userid});
 
   factory Friend.fromJson(Map<String, dynamic> json) {
     return Friend(
+      userid: json['idUsuario'],
       name: json['username'],
       score: json['puntaje'],
     );
@@ -26,12 +28,47 @@ class FriendsPage extends StatefulWidget {
 
 class _FriendsPageState extends State<FriendsPage> {
   List<Friend> friends = [];
+  List<Friend> searchResults = [];
   late Future<void> _friendsFuture;
+  TextEditingController _searchController = TextEditingController();
+  bool isSearching = false;
 
   @override
   void initState() {
     super.initState();
     _friendsFuture = fetchFriends();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  Future<bool> addFriendPost(friendId) async {
+    try {
+      final response = await http.post(
+        Uri.parse('${dotenv.env['API_URL']}/friends/add'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': '${dotenv.env['API_KEY']}',
+        },
+        body: jsonEncode({
+          'id_usuario': Supabase.instance.client.auth.currentSession!.user.id,
+          'id_amigo': friendId,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print('Failed to add friend: $e');
+      throw false;
+    }
   }
 
   Future<void> fetchFriends() async {
@@ -49,6 +86,8 @@ class _FriendsPageState extends State<FriendsPage> {
         List<Friend> fetchedFriends = [];
         List<dynamic> userFriends = jsonDecode(responseUserFriends.body);
 
+        print(userFriends);
+
         for (var friend in userFriends) {
           fetchedFriends.add(Friend.fromJson(friend));
         }
@@ -61,6 +100,64 @@ class _FriendsPageState extends State<FriendsPage> {
     } catch (e) {
       print('Failed to load friends: $e');
     }
+  }
+
+  Future<void> searchFriends(String query) async {
+    try {
+      final response = await http.get(
+        Uri.parse('${dotenv.env['API_URL']}/user/like_username/$query'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': '${dotenv.env['API_KEY']}',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        List<dynamic> searchResultsJson = jsonDecode(response.body);
+        List<Friend> fetchedFriends =
+            searchResultsJson.map((json) => Friend.fromJson(json)).toList();
+        // Remove friends that are already in the friends list
+        fetchedFriends.removeWhere((friend) => friends
+            .any((existingFriend) => existingFriend.userid == friend.userid));
+        setState(() {
+          searchResults = fetchedFriends;
+        });
+      } else {
+        print('Failed to search friends');
+      }
+    } catch (e) {
+      print('Failed to search friends: $e');
+    }
+  }
+
+  void _onSearchChanged() {
+    final query = _searchController.text.trim();
+    if (query.isNotEmpty) {
+      setState(() {
+        isSearching = true;
+      });
+      searchFriends(query);
+    } else {
+      setState(() {
+        isSearching = false;
+        searchResults = [];
+      });
+    }
+  }
+
+  Future<void> addFriend(Friend friend) async {
+    // Mockup of the fetch, replace with actual API call
+    bool added = await addFriendPost(friend.userid);
+
+    if (!added) {
+      return;
+    }
+
+    setState(() {
+      friends.add(friend);
+      isSearching = false;
+      _searchController.clear();
+    });
   }
 
   @override
@@ -83,8 +180,22 @@ class _FriendsPageState extends State<FriendsPage> {
               children: [
                 Center(
                   child: Text(
-                    'Amigos', 
+                    'Amigos',
                     style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Buscar amigos...',
+                    prefixIcon: Icon(Icons.search),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
                 ),
                 SizedBox(height: 16),
@@ -102,34 +213,53 @@ class _FriendsPageState extends State<FriendsPage> {
                       } else if (snapshot.hasError) {
                         return Center(child: Text('Error loading friends'));
                       } else {
+                        final displayList =
+                            isSearching ? searchResults : friends;
                         return ListView.builder(
-                          itemCount: friends.length,
+                          itemCount: displayList.length,
                           itemBuilder: (context, index) {
-                            final friend = friends[index];
+                            final friend = displayList[index];
                             return Card(
-                              margin: EdgeInsets.symmetric(vertical: 8), // Adjusted margin
+                              margin: EdgeInsets.symmetric(
+                                  vertical: 8), // Adjusted margin
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               elevation: 5,
                               child: ListTile(
-                                contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8), // Adjusted padding
-                                leading: UserAvatar(name: friend.name), // Added UserAvatar
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 16,
+                                    vertical: 8), // Adjusted padding
+                                leading: UserAvatar(
+                                    name: friend.name), // Added UserAvatar
                                 title: Text(
                                   friend.name,
-                                  style: TextStyle(fontSize: 20, color: Colors.black), // Adjusted font size and color
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors
+                                          .black), // Adjusted font size and color
                                 ),
-                                trailing: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Icon(Icons.monetization_on, color: Colors.yellow),
-                                    SizedBox(width: 4),
-                                    Text(
-                                      '${friend.score}',
-                                      style: TextStyle(fontSize: 20, color: Colors.black), // Adjusted font size and color
-                                    ),
-                                  ],
-                                ),
+                                trailing: isSearching
+                                    ? IconButton(
+                                        icon: Icon(Icons.add,
+                                            color: Colors.green),
+                                        onPressed: () => addFriend(friend),
+                                      )
+                                    : Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(Icons.monetization_on,
+                                              color: Colors.yellow),
+                                          SizedBox(width: 4),
+                                          Text(
+                                            '${friend.score}',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                color: Colors
+                                                    .black), // Adjusted font size and color
+                                          ),
+                                        ],
+                                      ),
                               ),
                             );
                           },
